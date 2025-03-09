47904415090163c556a5b81c7234f8e2f017baae
son12039 <sonson12039@gmail.com>
2025-03-09 15:43:25 +0900

    1

--- dist/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _react = require("react");
-var _client = require("react-dom/client");
-require("./index.css");
-var _App = _interopRequireDefault(require("./App.jsx"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-(0, _client.createRoot)(document.getElementById('root')).render(/*#__PURE__*/React.createElement(_react.StrictMode, null, /*#__PURE__*/React.createElement(_App["default"], null)));
